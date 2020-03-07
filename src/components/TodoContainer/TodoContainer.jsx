a04fda87abdfec671b1ddee312373422f1e7f58e
Taras <tpostolyuk@gmail.com>
2020-03-07 16:42:47 +0200

    Small changes

--- src/components/TodoContainer/TodoContainer.jsx
+++ src/components/TodoContainer/TodoContainer.jsx
@@ -1,17 +1,11 @@
 import React from 'react';
-import  s  from './TodoContainer.module.scss';
+import s from './TodoContainer.module.scss';
 import TodoBoard from '../TodoBoard/TodoBoard.jsx';
-import Switch from '@material-ui/core/Switch';
 
-export const TodoContainer = ({handleDarkMode}) => {
+export const TodoContainer = () => {
   return (
     <div className={s.todoContainer}>
       <div className={s.todoHeader}>
-        <Switch
-          onChange={handleDarkMode}
-          value="checkedA"
-          inputProps={{ 'aria-label': 'secondary checkbox' }}
-        />
         <h1>React To-Do App</h1>
       </div>
       <div className={s.todoBoard}>
